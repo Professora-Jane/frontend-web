1771bb6a46e2c23fc752028ceaf6e7f9925dbef5
Elias Bernardo <ebmm01@gmail.com>
2020-12-27 00:18:36 -0300

    Adding schedule view

---
+++ src/views/schedule/Schedule.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="schedule">
+        <v-calendar
+            ref="calendar"
+            v-model="value"
+            :weekdays="weekday"
+            :type="type"
+            hide-header
+            :interval-format="intervalFormat"
+            :first-interval="firstInterval"
+            :interval-count="15" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: undefined,
+            type: 'week',
+            mode: 'stack',
+            weekday: [0, 1, 2, 3, 4, 5, 6],
+            events: [],
+            firstInterval: 6
+        }
+    },
+    methods: {
+        intervalFormat(interval) {
+            return interval.time
+        }
+    }
+}
+</script>
