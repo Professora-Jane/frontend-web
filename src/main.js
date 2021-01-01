6b0cff5e9008bd02b6922b697e0952e621342c08
Elias Bernardo <ebmm01@gmail.com>
2021-01-01 18:44:02 -0300

    Integrating webscocket layer in vuejs

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import { socketInstance } from './websockets/socketClient';
+import { socketHandlerInstance } from './websockets/socketHandler';
 
 Vue.config.productionTip = false
+
+Vue.prototype.$wsEmit = (eventName, content) => socketInstance.send(eventName, content)
+
+socketInstance
+    .initConnection({ wsServer: "ws://localhost:7112" })
+    .registerOnMessageHandler(socketHandlerInstance.messageHandler)
 
 new Vue({
 	router,
