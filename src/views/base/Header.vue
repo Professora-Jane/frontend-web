16b83f72ed4b968917280bfe4f507ec4a12b2746
Elias Bernardo <ebmm01@gmail.com>
2021-01-09 23:52:14 -0300

    Adding menu to header with logout option

--- src/views/base/Header.vue
+++ src/views/base/Header.vue
@@ -18,23 +18,83 @@
                     mdi-bell
                 </v-icon>
             </v-btn>
-            <v-btn
-                href="#"
-                icon>
-                <v-icon>
-                    mdi-account
-                </v-icon>
-            </v-btn>
+            <v-menu 
+                attach="#base-header"
+                content-class="user-settings"
+                left
+                offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                        icon
+                        v-bind="attrs"
+                        v-on="on">
+                        <v-icon>
+                            mdi-account
+                        </v-icon>
+                    </v-btn>
+                </template>
+                <div 
+                    class="d-flex py-2 px-3 align-center">
+                    <img 
+                        class="header-profile-img"
+                        :src="userImg">
+                    <div>
+                        <p class="body-2 mb-0">
+                            {{ name }}
+                        </p>
+                        <p class="caption mb-0">
+                            {{ email }}
+                        </p>
+                    </div>
+                </div>
+                <v-divider />
+                <v-list>
+                    <!-- <v-list-item
+                        class="header-list-item px-2">
+                        <v-list-item-title>
+                            <v-switch
+                                inset
+                                v-model="darkTheme"
+                                hide-details=""
+                                label="Tema escuro"
+                                @change="updateDarkTheme" />
+                        </v-list-item-title>
+                    </v-list-item> -->
+                    <v-list-item
+                        v-ripple
+                        v-for="(item, index) in items"
+                        :key="index"
+                        class="header-list-item px-2">
+                        <v-list-item-title
+                            @click="item.handler">
+                            <v-icon
+                                class="mr-1">
+                                {{ item.icon }}
+                            </v-icon>
+                            {{ item.title }}
+                        </v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
         </v-container>
     </v-app-bar>
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex"
+
 export default {
     data() {
         return {
             title: undefined,
-            isScrolled: false
+            isScrolled: false,
+            userProfileImg: undefined,
+            items: [
+                { title: "Editar perfil", icon: "mdi-account-cog", handler: () => {} },
+                { title: "Configurações", icon: "mdi-cog", handler: () => {}},
+                { title: "Sair", icon: "mdi-logout-variant", handler: this.logoutUserHandler},
+            ],
+            darkTheme: false
         }
     },
     watch: {
@@ -42,13 +102,64 @@ export default {
             this.title = value.meta.title
         }
     },
+    computed: {
+        ...mapState('authUser', [
+            'name',
+            'email'
+        ]),
+        userImg() {
+            return this.userProfileImg ||  require("../../assets/images/default_user_img.png")
+        }
+    },
     methods: {
+        ...mapActions("authUser", ["logoutUser"]),
         showScroll() {
             this.isScrolled = document.documentElement.scrollTop > 50
+        },
+        logoutUserHandler() {
+            this.logoutUser()
+            debugger
+            this.$router.push({ name: "login" })
+        },
+        updateDarkTheme() {
+            localStorage.setItem("darkTheme", `${!this.darkTheme}`)
+            this.darkTheme = !this.darkTheme
+            debugger
+            this.$vuetify.theme.dark = this.darkTheme
         }
     },
     created() {
         this.title = this.$router.currentRoute.meta.title
+        this.darkTheme = localStorage.getItem("darkTheme") == "true" || false
+        this.$vuetify.theme.dark = this.darkTheme
     }
 }
 </script>
+
+<style lang="scss">
+#base-header {
+    .user-settings {
+        background: $c-white;
+
+        .header-profile-img {
+            width: 30px;
+            height: 30px;
+            border-radius: 50%;
+            margin-right: 10px;
+        }
+
+        .header-list-item {
+            cursor: pointer;
+            transition: all 0.2s ease-out;
+
+            &:hover {
+                background: rgba(0, 0, 0, 0.1);
+            }
+
+            .v-list-item__title {
+                line-height: 1.5;
+            }
+        }
+    }
+}
+</style>
