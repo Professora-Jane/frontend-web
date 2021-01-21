027291ce3a9e51631a24d1edb09cd05daa7b4dba
Elias Bernardo <ebmm01@gmail.com>
2021-01-21 13:28:22 -0300

    Updating rom Service

--- src/services/RoomService.js
+++ src/services/RoomService.js
@@ -38,10 +38,16 @@ export default class RoomService {
      * @param { number } [params.page] - Página atual
      * @param { number } [params.limit] - Limite de itens por página
      * @param { string } params.adminId - ObjectId do admin da sala
+     * @param { string } params.search - String de busca
+     * @param { string } params.sort - Chave de ordenação
+     * @param { number } params.sortType - Tipo de ordenação
      */
-    async listFinishedRooms({ adminId, page = 1, limit = 20 }) {
+    async listFinishedRooms({ adminId, page = 1, limit = 20, search = "", sort = "", sortType = 0}) {
         
-        const response = await this.client.get(`/room/list/finished/${adminId}?page=${page}&limit=${limit}`)
+        const searchQuery = search? `&search=${search}` : ""
+        const sortQuery = sort? `&sort=${sort}` : ""
+        const sortTypeQuery = sortType? `&sortType=${sortType}` : ""
+        const response = await this.client.get(`/room/list/finished/${adminId}?page=${page}&limit=${limit}${searchQuery}${sortQuery}${sortTypeQuery}`)
 
         return response
     }
