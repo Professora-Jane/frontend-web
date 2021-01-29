fec1d32d6dcdeff6e107f9dead2ef53615d962bf
Elias Bernardo <ebmm01@gmail.com>
2021-01-29 20:45:59 -0300

    Updating routes

--- src/router/index.js
+++ src/router/index.js
@@ -10,14 +10,9 @@ const routes = [
 		name: 'base',
         component: () => import(/* webpackChunkName: "base_component" */ '../views/base/BaseComponent.vue'),
         children: [
-            {
-                path: 'home',
-                name: 'home',
-                meta: {
-                    title: "Home page"
-                },
-                component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'), 
-            },
+            /**
+             * TEACHER ROUTES
+             */
             {
                 path: 'schedule',
                 name: 'schedule',
@@ -25,7 +20,7 @@ const routes = [
                     title: "Agenda"
                 },
                 beforeEnter: (to,from, next) => requireTeacherAuth(to, from, next),
-                component: () => import(/* webpackChunkName: "schedule" */ '../views/schedule/Schedule.vue'), 
+                component: () => import(/* webpackChunkName: "schedule" */ '../views/teacherViews/schedule/Schedule.vue'), 
             },
             {
                 path: 'students',
@@ -34,7 +29,7 @@ const routes = [
                     title: "Estudantes"
                 },
                 beforeEnter: (to,from, next) => requireTeacherAuth(to, from, next),
-                component: () => import(/* webpackChunkName: "students" */ '../views/students/Students.vue'), 
+                component: () => import(/* webpackChunkName: "students" */ '../views/teacherViews/students/Students.vue'), 
             },
             {
                 path: 'student/detail/:id',
@@ -43,8 +38,46 @@ const routes = [
                     title: "Detalhes do estudante"
                 },
                 beforeEnter: (to,from, next) => requireTeacherAuth(to, from, next),
-                component: () => import(/* webpackChunkName: "student_detail" */ '../views/students/StudentDetails.vue'), 
+                component: () => import(/* webpackChunkName: "student_detail" */ '../views/teacherViews/students/StudentDetails.vue'), 
             },
+            /**
+             * END OF TEACHER ROUTES
+             */
+
+            /** 
+             * STUDENTS ROUTES
+             */
+
+            {
+                path: 'student/schedule',
+                name: 'studentSchedule',
+                meta: {
+                    title: "Minhas aulas"
+                },
+                beforeEnter: (to,from, next) => requireStudentAuth(to, from, next),
+                component: () => import(/* webpackChunkName: "student_schedule" */ '../views/studentViews/schedule/Schedule.vue'), 
+            },
+            {
+                path: 'student/teacher/list',
+                name: 'studentTeachers',
+                meta: {
+                    title: "Meus professores"
+                },
+                beforeEnter: (to,from, next) => requireStudentAuth(to, from, next),
+                component: () => import(/* webpackChunkName: "student_teachers" */ '../views/studentViews/myTeachers/Teachers.vue'), 
+            },
+            {
+                path: 'student/teacher/:id',
+                name: 'studentTeacherDetails',
+                meta: {
+                    title: "Detalhes do professor"
+                },
+                beforeEnter: (to,from, next) => requireStudentAuth(to, from, next),
+                component: () => import(/* webpackChunkName: "student_teacher_details" */ '../views/studentViews/myTeachers/TeacherDetails.vue'), 
+            },
+            /**
+             * END OF STUDENTS ROUTES  
+             */
             {
                 path: 'room',
                 name: 'room',
@@ -62,7 +95,15 @@ const routes = [
                 },
                 beforeEnter: requireAuth,
                 component: () => import(/* webpackChunkName: "room" */ '../views/room/Room.vue'), 
-            }
+            },
+            {
+                path: 'home',
+                name: 'home',
+                meta: {
+                    title: "Home page"
+                },
+                component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'), 
+            },
         ]
     },
     {
@@ -90,7 +131,6 @@ const routes = [
 ]
 
 const requireTeacherAuth = (to, from, next) => {
-    console.log(store.state)
     if (store.state.authUser.token && store.state.authUser.type === "professor") {
         next()
         return
@@ -103,16 +143,16 @@ const requireAuth = (to, from, next) => {
         next()
         return
     }
-    next('/login')
+    next('/auth/login')
 }
 
-// const requireStudentAuth = (to, from, next) => {
-//     if (store.state.authUser.token && store.state.authUser.type === "aluno") {
-//         next()
-//         return
-//     }
-//     next('/login')
-// }
+const requireStudentAuth = (to, from, next) => {
+    if (store.state.authUser.token && store.state.authUser.type === "aluno") {
+        next()
+        return
+    }
+    next('/login')
+}
 
 const router = new VueRouter({
 	mode: 'history',
